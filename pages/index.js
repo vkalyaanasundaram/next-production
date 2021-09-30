a7d93b377bc7b564c8deee48d4ceff41798c3924
Vinoth <vkalyaanasundaram@kapitus.com>
2021-09-30 09:22:37 +0530

    header and footer splited and imported in index

--- pages/index.js
+++ pages/index.js
@@ -1,206 +1,144 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-   <>
-   <header className="text-gray-600 body-font">
-    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-      <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
-        <span className="ml-3 text-xl">Demo</span>
-      </a>
-      <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-        <a className="mr-5 hover:text-gray-900">First Link</a>
-        <a className="mr-5 hover:text-gray-900">Second Link</a>
-        <a className="mr-5 hover:text-gray-900">Third Link</a>
-        <a className="mr-5 hover:text-gray-900" href="/blogs">Blogs</a>
-      </nav>
-      {/* <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-          <path d="M5 12h14M12 5l7 7-7 7"></path>
-        </svg>
-      </button> */}
-    </div>
-  </header>
-<section>
-<div class="rounded-lg overflow-hidden">
-    <img alt="content" class="w-full" height="600" src="./HeroImages_secondarypage_salespartners.jpg" />
-  </div>
-</section>
-<section>
-  <div className="container px-5 py-24 mx-auto">
-    <h3 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-6 mb-6 text-center">
-      Explore by Popular Topics
-    </h3>
-    <div className="grid grid-cols-3 gap-4">
-      <button class="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">LEADERSHIP</button>
-      <button class="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">GETTING CUSTOMERS</button>
-      <button class="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">Upcoming Events</button>
-      <button class="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">Managing Money</button>
-      <button class="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">Office Hours</button>
-      <button class="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">Cash Flow</button>
-    </div>
-  </div>
-</section>
-<section className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto">
-    <h3 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">What's New This Week</h3>
-  </div>
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap w-full mb-20">
-      <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
-        <div class="h-1 w-20 bg-indigo-500 rounded"></div>
-      </div>
-      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
-    </div>
-    <div class="flex flex-wrap -m-4">
-      <div class="xl:w-1/3 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
-          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">Saving Money</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Money Saving Tips and Ideas for Small Businesses </h2>
-          <p class="leading-relaxed text-base">Annie Mueller.</p>
+    <>
+      {/* Header  */}
+      <Header />
+      <section>
+        <div className="rounded-lg overflow-hidden">
+          <img
+            alt="content"
+            className="w-full"
+            height="auto"
+            src="./HeroImages_secondarypage_salespartners.jpg"
+          />
         </div>
-      </div>
-      <div class="xl:w-1/3 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content" />
-          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
-          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-        </div>
-      </div>
-      <div class="xl:w-1/3 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content" />
-          <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
-          <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  
-</section>
-<section class="text-gray-600 body-font text-center">
-  <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
-    <div class="lg:w-full md:w-full md:pr-16 lg:pr-0 pr-0">
-      <h1 class="title-font font-medium text-3xl text-gray-900">Slow-carb next level shoindcgoitch ethical authentic, poko scenester</h1>
-      <p class="leading-relaxed mt-4">Poke slow-carb mixtape knausgaard, typewriter street art gentrify hammock starladder roathse. Craies vegan tousled etsy austin.</p>
-    </div>
-  </div>
-</section>
-<footer className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-wrap md:text-left text-center order-first">
-      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-        <nav className="list-none mb-10">
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">First Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-          </li>
-        </nav>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-        <nav className="list-none mb-10">
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">First Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-          </li>
-        </nav>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-        <nav className="list-none mb-10">
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">First Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-          </li>
-          <li>
-            <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-          </li>
-        </nav>
-      </div>
-      <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
-        <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
-          <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
-            <label htmlFor="footer-field" className="leading-7 text-sm text-gray-600">Placeholder</label>
-            <input type="text" id="footer-field" name="footer-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+      </section>
+      <section>
+        <div className="container px-5 py-24 mx-auto">
+          <h3 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-6 mb-6 text-center">
+            Explore by Popular Topics
+          </h3>
+          <div className="grid grid-cols-3 gap-4">
+            <button className="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">
+              LEADERSHIP
+            </button>
+            <button className="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">
+              GETTING CUSTOMERS
+            </button>
+            <button className="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">
+              Upcoming Events
+            </button>
+            <button className="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">
+              Managing Money
+            </button>
+            <button className="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">
+              Office Hours
+            </button>
+            <button className="text-black bg-gray-100 border-0 py-8 px-8 uppercase focus:outline-none hover:bg-indigo-600 hover:text-white rounded text-lg">
+              Cash Flow
+            </button>
           </div>
-          <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
         </div>
-        <p className="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
-          <span className="lg:block hidden">waistcoat green juice</span>
-        </p>
-      </div>
-    </div>
-  </div>
-  
-  <div className="bg-gray-100">
-    <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-      <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-        </svg>
-        <span className="ml-3 text-xl">Tailblocks</span>
-      </a>
-      <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
-        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
-      </p>
-      <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-        <a className="text-gray-500">
-          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-          </svg>
-        </a>
-        <a className="ml-3 text-gray-500">
-          <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-          </svg>
-        </a>
-        <a className="ml-3 text-gray-500">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-          </svg>
-        </a>
-        <a className="ml-3 text-gray-500">
-          <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
-            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-            <circle cx="4" cy="4" r="2" stroke="none"></circle>
-          </svg>
-        </a>
-      </span>
-    </div>
-  </div>
-</footer>
-</>
-)
+      </section>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <h3 className="sm:text-3xl text-2xl title-font font-medium text-gray-900 mt-4 mb-4">
+            What's New This Week
+          </h3>
+        </div>
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap w-full mb-20">
+            <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+                Pitchfork Kickstarter Taxidermy
+              </h1>
+              <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+            </div>
+            <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              haven't heard of them man bun deep jianbing selfies heirloom prism
+              food truck ugh squid celiac humblebrag.
+            </p>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            <div className="xl:w-1/3 md:w-1/2 p-4">
+              <div className="bg-gray-100 p-6 rounded-lg">
+                <img
+                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/720x400"
+                  alt="content"
+                />
+                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                  Saving Money
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Money Saving Tips and Ideas for Small Businesses{" "}
+                </h2>
+                <p className="leading-relaxed text-base">Annie Mueller.</p>
+              </div>
+            </div>
+            <div className="xl:w-1/3 md:w-1/2 p-4">
+              <div className="bg-gray-100 p-6 rounded-lg">
+                <img
+                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/721x401"
+                  alt="content"
+                />
+                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Colosseum Roma
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+            <div className="xl:w-1/3 md:w-1/2 p-4">
+              <div className="bg-gray-100 p-6 rounded-lg">
+                <img
+                  className="h-40 rounded w-full object-cover object-center mb-6"
+                  src="https://dummyimage.com/722x402"
+                  alt="content"
+                />
+                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                  SUBTITLE
+                </h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                  Great Pyramid of Giza
+                </h2>
+                <p className="leading-relaxed text-base">
+                  Fingerstache flexitarian street art 8-bit waistcoat.
+                  Distillery hexagon disrupt edison bulbche.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="text-gray-600 body-font text-center">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
+          <div className="lg:w-full md:w-full md:pr-16 lg:pr-0 pr-0">
+            <h1 className="title-font font-medium text-3xl text-gray-900">
+              Slow-carb next level shoindcgoitch ethical authentic, poko
+              scenester
+            </h1>
+            <p className="leading-relaxed mt-4">
+              Poke slow-carb mixtape knausgaard, typewriter street art gentrify
+              hammock starladder roathse. Craies vegan tousled etsy austin.
+            </p>
+          </div>
+        </div>
+      </section>
+      {/* Footer  */}
+      <Footer />
+    </>
+  );
 }
